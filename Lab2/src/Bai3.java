9b6cd96cfc7b5b2760e2c530ec33ae891d392564
LTK2102 <tuankiet5505@gmail.com>
2024-01-25 11:26:33 +0700

    Bài 2 và Bài 3

---
+++ Lab2/src/Bai3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Bai3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Nhập số điện sử dụng của tháng
+        System.out.print("Nhập số điện sử dụng của tháng: ");
+        int soDien = scanner.nextInt();
+
+        // Tính tiền điện
+        int giaDuoi50 = 1000;
+        int giaTren50 = 1200;
+        int gioiHan50 = 50;
+
+        int tien;
+
+        if (soDien <= gioiHan50) {
+            tien = soDien * giaDuoi50;
+        } else {
+            tien = gioiHan50 * giaDuoi50 + (soDien - gioiHan50) * giaTren50;
+        }
+
+        // Hiển thị kết quả
+        System.out.println("Tiền điện phải trả: " + tien + " VND");
+
+        scanner.close();
+    }
+}
